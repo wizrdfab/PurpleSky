c2aa41345cabb87de7947d71afa05599a1489053
Fabb <fgzf.mm@gmail.com>
2026-01-13 14:56:27 -0300

    Update core trading and strategy files

--- backtest.py
+++ backtest.py
@@ -1,5 +1,5 @@
 """
-Copyright (C) 2026 [YOUR NAME HERE]
+Copyright (C) 2026 Fabián Zúñiga Franck
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as published
